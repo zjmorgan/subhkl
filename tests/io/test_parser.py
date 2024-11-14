554fd27fce462bc6fb79f4e49bc562dbb3c70bd9
Robert Smith <smithrw@ornl.gov>
2024-11-14 11:46:44 -0500

    Fixed parser test input file location
    Fixed the tif input file location for the parser test

--- tests/io/test_parser.py
+++ tests/io/test_parser.py
@@ -4,7 +4,7 @@ from subhkl.io.parser import app
 
 def test_find_args():
     runner = CliRunner()
-    test_args = ["find", "my.tiff"]
+    test_args = ["finder", "tests/meso_2_15min_2-0_4-5_050.tif", "out.csv"]
     result = runner.invoke(app, test_args)
     assert result.exit_code == 0
 
