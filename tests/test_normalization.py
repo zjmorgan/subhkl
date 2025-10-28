ed13d058d8871989858b80d2d30966d10252fb04
SmithRWORNL <smithrw@ornl.gov>
2025-10-28 10:23:04 -0400

    Added lorentz correction to normalization module
    Also added a normalize command to the parser to run it.

---
+++ tests/test_normalization.py
@@ -0,0 +1,18 @@
+from subhkl.normalization import lorentz_correction
+from subhkl.parser import normalize
+
+def test_lorentz_correction():
+    '''
+    Test the lorentz correction.
+    '''
+    pass
+    #assert 0 = lorentz_correction(0, 1)
+    
+def test_parser():
+    '''
+    Test the parser normalization command.
+    '''
+    pass
+    #normalize(input_file, "out.hdf5")
+    
+    
