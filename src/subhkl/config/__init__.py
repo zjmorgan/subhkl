e5ba79b3463f7392d1c55f529f481e27e30c2805
Jacob Hauck <hauckjp@gmail.com>
2025-10-28 12:41:10 -0400

    Add subhkl.config.calc_goniometer_rotation_matrix function (works the same as Mantid SetGoniometer)

--- src/subhkl/config/__init__.py
+++ src/subhkl/config/__init__.py
@@ -1,6 +1,8 @@
 from .config import beamlines, reduction_settings
+from .goniometer import calc_goniometer_rotation_matrix
 
 __all__ = [
     "beamlines",
     "reduction_settings",
+    "calc_goniometer_rotation_matrix",
 ]
