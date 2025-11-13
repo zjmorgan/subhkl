622e05170e57415294921d793ace1c2d3a8cb3b8
Jens Glaser <glaserj@ornl.gov>
2025-11-13 12:09:07 -0500

    forgot to write out bank info in h5

--- src/subhkl/io/parser.py
+++ src/subhkl/io/parser.py
@@ -392,6 +392,7 @@ def integrator(
         f["peaks/sigma"] = result.sigma
         f["peaks/scattering"] = result.tt
         f["peaks/azimuthal"] = result.az
+        f["peaks/bank"] = result.bank
 
         with h5py.File(integration_peaks_filename) as f_in:
             for key in copy_keys:
