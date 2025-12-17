35b70cc24c26c1ab5fd58b6fc38141d0065ea827
Jacob Hauck <jacobh15@yahoo.com>
2025-12-17 11:31:18 -0500

    Leave warning suppression to other branch

--- src/subhkl/integration.py
+++ src/subhkl/integration.py
@@ -549,8 +549,7 @@ class Peaks:
         hkl = [h.flatten(), k.flatten(), l.flatten()]
         h, k, l = hkl
 
-        with np.errstate(divide='ignore'):
-            d = 1 / np.sqrt(np.einsum("ij,jl,il->l", Gstar, hkl, hkl))
+        d = 1 / np.sqrt(np.einsum("ij,jl,il->l", Gstar, hkl, hkl))
 
         mask = (d > d_min) & (d < np.inf)
 
