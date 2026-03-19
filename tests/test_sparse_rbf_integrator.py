89f8d2c435be92ee8c3edd8fa6116c5ab247f936
Jens Glaser <glaserj@ornl.gov>
2026-03-18 20:38:26 -0400

    mock panel type

--- tests/test_sparse_rbf_integrator.py
+++ tests/test_sparse_rbf_integrator.py
@@ -145,10 +145,9 @@ def test_integrate_peaks_rbf_ssn_orchestrator():
                         "height": H * 1.0,
                         "pixel_size": 1.0,
                         "center": [0.0, 0.0, 100.0],
-                        "fast_axis": [1.0, 0.0, 0.0],
-                        "slow_axis": [0.0, -1.0, 0.0],
                         "uhat": [1.0, 0.0, 0.0],
                         "vhat": [0.0, 1.0, 0.0],
+                        "panel": "flat",
                     }
                 }
             }
