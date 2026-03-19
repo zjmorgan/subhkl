2b0dc397d0ac6dcee8ccd6b3d62ae6f272e62557
Jens Glaser <glaserj@ornl.gov>
2026-03-19 18:14:36 -0400

    Add hough transform file name option

--- src/subhkl/io/parser.py
+++ src/subhkl/io/parser.py
@@ -1312,6 +1312,7 @@ def zone_axis_search(
     davenport_angle_tol: float = typer.Option(0.5, help="Graph search angle tolerance in degrees."),
     prior_top_k_rays: int = typer.Option(15, help="Max rays per image to feed the Hough Transform."),
     num_runs: int = typer.Option(1, help="Number of goniometer runs to use. Set to 0 to use all.")
+    output_hough: str = typer.Option(None, help="Diagnostic hough transform image filename.")
 ):
     """
     Global Zone-Axis Search to find the macroscopic crystal orientation (U matrix).
@@ -1404,7 +1405,8 @@ def zone_axis_search(
                                                 top_k_rays=prior_top_k_rays)
 
     print(f"Extracted {len(q_hat)} physical rays. Running 3D Combinatorial Hough...")
-    n_obs, weights_obs = prior_engine.compute_hough_accumulator(q_hat, grid_resolution=hough_grid_resolution)
+    n_obs, weights_obs = prior_engine.compute_hough_accumulator(q_hat, grid_resolution=hough_grid_resolution,
+            plot_filename=output_hough)
 
     if len(n_obs) == 0:
         print("Failed to find any zone axes.")
