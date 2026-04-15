957a0f1317f2b8ca852f4bb755fb6c5760c96c0f
Jens Glaser <glaserj@ornl.gov>
2026-04-15 15:22:37 -0400

    Fix formatting

--- src/subhkl/optimization.py
+++ src/subhkl/optimization.py
@@ -468,7 +468,9 @@ class VectorizedObjective:
         if self.refine_goniometer:
             gonio_norm = jnp.full((x.shape[0], self.num_gonio_axes), 0.5)
             if self.num_active_gonio > 0:
-                gonio_norm = gonio_norm.at[(slice(None), self.gonio_mask)].set(x[:, idx : idx + self.num_active_gonio])
+                gonio_norm = gonio_norm.at[(slice(None), self.gonio_mask)].set(
+                    x[:, idx : idx + self.num_active_gonio]
+                )
                 idx += self.num_active_gonio
 
             offsets_delta = _forward_map_param(gonio_norm, self.goniometer_bound_deg)
